8349438d467f903533e9aad52d63b9c067eb936e
PapaDev <papadev@PapaDevs-MacBook-Air.local>
2026-01-27 12:05:28 -0500

    chore(vendor): remove stale compiled page artifacts

--- apps/vendor-app/src/pages/vendor/dashboard/page.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var page_1 = require("../../../../customer-app/src/pages/vendor/dashboard/page");
-exports.default = page_1.default;
