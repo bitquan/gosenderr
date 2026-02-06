b56a888ca2b59919a852533a7ed715e729a17273
PapaDev <papadev@PapaDevs-MacBook-Air.local>
2026-02-06 14:46:43 -0500

    chore(courieriosnativeclean): align eslint deps with lockfile

--- apps/courieriosnativeclean/.eslintrc.js
+++ apps/courieriosnativeclean/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // Disable problematic rule until ESLint/@typescript-eslint toolchain is normalized across the mono-repo
+    '@typescript-eslint/no-unused-expressions': 'off',
+  },
 };
